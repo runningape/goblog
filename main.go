81fb7b4f4656e49e19cc9653bb05c35adf2db039
RunningHuang <running.huang@qq.com>
2024-02-20 16:55:23 +0800

    初始化

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>welcome to my goblog!")
+	fmt.Fprintf(w, "<h1>welcome to goblog</h1>")
 }
 
 func main() {
