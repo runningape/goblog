edf9353d40f6ac93e3ca324057f16cebae6101be
runningape <runningape@qq.com>
2024-02-21 21:29:40 +0800

    新建博客表单

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var router = mux.NewRouter()
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello,这里是goblog</h1>")
 }
@@ -35,6 +37,25 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "创建新的文章")
 }
 
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	html := `
+		<!DOCTYPE html>
+			<head>
+				<title>创建文章 - 我的技术博客</title>
+			</head>
+			<body>
+				<form action="%s" method="post">
+					<p><input type="text" name="title"></p>
+					<p><textarea name="body" cols="30" rows="10"></textarea>
+					<p><button type="submit">提交</button></p>
+				</form>
+			</body>
+		</html>
+	`
+	storeURL, _ := router.Get("articles.store").URL()
+	fmt.Fprintf(w, html, storeURL)
+}
+
 func forceHTMLMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -52,8 +73,6 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 }
 
 func main() {
-	router := mux.NewRouter()
-
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
@@ -63,13 +82,10 @@ func main() {
 		articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles",
 		articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/create",
+		articlesCreateHandler).Methods("GET").Name("articles.create")
 
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
-
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
-	fmt.Println("articleURL: ", articleURL)
 
 	router.Use(forceHTMLMiddleware)
 
