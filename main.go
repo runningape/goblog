43b7c3498abec7898b7e4dfe17b03abd6572395c
runningape <runningape@qq.com>
2024-02-21 20:24:36 +0800

    使用中间件

--- main.go
+++ main.go
@@ -8,17 +8,14 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>welcome to goblog</h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(w, "about page")
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>您访问的页面不存在。。。</h1><p>Please contact me</p>")
 }
@@ -35,6 +32,13 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "创建新的文章")
+}
+
+func forceHTMLMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
@@ -56,6 +60,8 @@ func main() {
 	fmt.Println("homeURL:", homeURL)
 	articlesURL, _ := router.Get("articles.show").URL("id", "888")
 	fmt.Println("articlesURL:", articlesURL)
+
+	router.Use(forceHTMLMiddleware)
 
 	http.ListenAndServe(":3000", router)
 }
