ed5a9180f4e572e5e7eae46b3cad73256ed0b6c3
RunningHuang <running.huang@qq.com>
2024-02-21 11:34:56 +0800

    尝试http.ServeMux

--- main.go
+++ main.go
@@ -5,19 +5,35 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprintf(w, "<h1>welcome to goblog</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprintf(w, "about page")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "not found page!")
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "about page")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+
+	router.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprintf(w, "访问文章列表")
+		case "POST":
+			fmt.Fprintf(w, "创建新的文章")
+		}
+	})
+
+	http.ListenAndServe(":3000", router)
 }
