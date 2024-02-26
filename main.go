9e295a5435c51ecf05f7600f35974175047d5f02
runningape <runningape@qq.com>
2024-02-26 21:21:09 +0800

    文章列表

--- main.go
+++ main.go
@@ -25,6 +25,15 @@ type ArticlesFormData struct {
 	Errors      map[string]string
 }
 
+func (a Article) Link() string {
+	showURL, err := router.Get("articles.show").URL("id", strconv.FormatInt(a.ID, 10))
+	if err != nil {
+		checkError(err)
+		return ""
+	}
+	return showURL.String()
+}
+
 type Article struct {
 	Title, Body string
 	ID          int64
@@ -94,7 +103,27 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "文章列表页面")
+	rows, err := db.Query("SELECT * FROM articles")
+	checkError(err)
+	defer rows.Close()
+
+	var articles []Article
+
+	for rows.Next() {
+		var article Article
+		err := rows.Scan(&article.ID, &article.Title, &article.Body)
+		checkError(err)
+		articles = append(articles, article)
+	}
+
+	err = rows.Err()
+	checkError(err)
+
+	tmpl, err := template.ParseFiles("resources/views/articles/index.html")
+	checkError(err)
+	err = tmpl.Execute(w, articles)
+	checkError(err)
+
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
