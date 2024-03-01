3cfc9d7a3baafcb9a0abce124916f13abd04863b
runningape <runningape@qq.com>
2024-03-01 15:56:55 +0800

    重构initDB 和 createTables

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 	"strings"
 	"text/template"
-	"time"
 	"unicode/utf8"
 
 	"github.com/runningape/goblog/logger"
@@ -16,7 +15,6 @@ import (
 	"github.com/runningape/goblog/pkg/route"
 	"github.com/runningape/goblog/pkg/types"
 
-	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
@@ -55,28 +53,6 @@ func (a Article) Delete() (rowsAffected int64, err error) {
 type Article struct {
 	Title, Body string
 	ID          int64
-}
-
-func initDB() {
-	var err error
-	config := mysql.Config{
-		User:                 "root",
-		Passwd:               "dyhuangZz223",
-		Addr:                 "127.0.0.1:3306",
-		Net:                  "tcp",
-		DBName:               "goblog",
-		AllowNativePasswords: true,
-	}
-
-	db, err = sql.Open("mysql", config.FormatDSN())
-	logger.LogError(err)
-
-	db.SetMaxOpenConns(25)
-	db.SetMaxIdleConns(25)
-	db.SetConnMaxLifetime(5 * time.Minute)
-
-	err = db.Ping()
-	logger.LogError(err)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -392,19 +368,6 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
-}
-
-func createTables() {
-	createArticlesSQL := `
-		CREATE TABLE IF NOT EXISTS articles(
-			id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-			title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-			body longtext COLLATE utf8mb4_unicode_ci
-		);
-	`
-
-	_, err := db.Exec(createArticlesSQL)
-	logger.LogError(err)
 }
 
 func main() {
