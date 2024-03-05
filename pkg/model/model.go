ffcf2d73c41310b1493d61439cc01a46935acf32
runningape <runningape@qq.com>
2024-03-05 11:51:10 +0800

    重构文章列表页面

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -4,6 +4,7 @@ import (
 	"github.com/runningape/goblog/pkg/logger"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -15,7 +16,9 @@ func ConnectDB() *gorm.DB {
 		DSN: "root:dyhuangZz223@tcp(127.0.0.1:3306)/goblog?charset=utf8&&parseTime=True&loc=Local",
 	})
 
-	DB, err = gorm.Open(config, &gorm.Config{})
+	DB, err = gorm.Open(config, &gorm.Config{
+		Logger: gormlogger.Default.LogMode(gormlogger.Warn),
+	})
 
 	logger.LogError(err)
 
