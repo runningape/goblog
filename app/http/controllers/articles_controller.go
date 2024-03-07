8da7976a89daacaacde7caed626fe98063881ed3
runningape <runningape@qq.com>
2024-03-07 15:20:33 +0800

    切割模板

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 	"strconv"
 	"text/template"
 	"unicode/utf8"
@@ -57,9 +58,13 @@ func (*ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, "500 Internal Server Error")
 	} else {
-		tmpl, err := template.ParseFiles("resources/views/articles/index.html")
+		viewDir := "resources/views"
+		files, err := filepath.Glob(viewDir + "/layouts/*.html")
 		logger.LogError(err)
-		err = tmpl.Execute(w, article)
+		newfiles := append(files, viewDir+"/articles/index.html")
+		tmpl, err := template.ParseFiles(newfiles...)
+		logger.LogError(err)
+		err = tmpl.ExecuteTemplate(w, "app", article)
 		logger.LogError(err)
 	}
 }
