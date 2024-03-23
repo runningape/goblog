a627620d6421eb602f34b67d7d7db4e29af3b34c
runningape <runningape@qq.com>
2024-03-24 00:08:09 +0800

    合话和认证管理

--- app/models/user/crud.go
+++ app/models/user/crud.go
@@ -1,10 +1,33 @@
 package user
 
-import "github.com/runningape/goblog/pkg/model"
+import (
+	"github.com/runningape/goblog/pkg/model"
+	"github.com/runningape/goblog/pkg/types"
+)
 
 func (user *User) Create() (err error) {
 	if err := model.DB.Create(&user).Error; err != nil {
 		return err
 	}
 	return nil
+}
+
+func Get(idstr string) (User, error) {
+	var user User
+
+	id := types.StringToUint64(idstr)
+
+	if err := model.DB.First(&user, id).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func GetByEmail(email string) (User, error) {
+	var user User
+
+	if err := model.DB.Where("email=?", email).First(&user).Error; err != nil {
+		return user, err
+	}
+	return user, nil
 }
