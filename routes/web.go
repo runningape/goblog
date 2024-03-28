73277f57ce45c0d81bf9845d41f0561a7a274299
runningape <runningape@qq.com>
2024-03-28 22:08:17 +0800

    退出登录

--- routes/web.go
+++ routes/web.go
@@ -11,7 +11,6 @@ import (
 func RegisterWebRoutes(r *mux.Router) {
 
 	pc := new(controllers.PagesController)
-
 	r.HandleFunc("/about", pc.About).Methods("GET").Name("about")
 	r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
 
@@ -30,6 +29,7 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.HandleFunc("/auth/do-register", auc.DoRegister).Methods("POST").Name("auth.doregister")
 	r.HandleFunc("/auth/login", auc.Login).Methods("GET").Name("auth.login")
 	r.HandleFunc("/auth/dologin", auc.DoLogin).Methods("POST").Name("auth.dologin")
+	r.HandleFunc("/auth/logout", auc.Logout).Methods("POST").Name("auth.logout")
 
 	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
 	r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
